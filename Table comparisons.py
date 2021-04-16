f2d3e38a042000a922b0948704c374a30bfa174a
sam <samslusky@gmail.com>
2021-04-15 21:39:06 -0400

    cont.

--- Table comparisons.py
+++ Table comparisons.py
@@ -11,12 +11,6 @@ raw_headers = ['kills', 'deaths', 'assists', 'largestkillingspree',
   'controlwardspurchased', 'goldearned', 'goldspent', 'minionskilled', 'neutralminionskilled', 
   "neutralminionskilledinteam'sjungle", 'neutralminionskilledinenemyjungle', 'url', 'champion']
 
-headers_editing = [, 'totaldamagetochampions',
-  'physicaldamagetochampions', 'magicdamagetochampions', 'truedamagetochampions', 
-  'totaldamagetoobjectives', 'totaldamagetoturrets', 'damagehealed', 'damagetaken', 
-  'magicdamagetaken', 'wardsdestroyed', 'stealthwardspurchased', 
-  'controlwardspurchased',  'minionskilled', 
-  "neutralminionskilledinteam'sjungle", 'neutralminionskilledinenemyjungle', 'url', 'champion']
 
 # format: raw header: my header
 headers_to_convert = {'firstBloodKill': 'firstblood',
@@ -35,88 +29,26 @@ headers_to_convert = {'firstBloodKill': 'firstblood',
 'totalMinionsKilled':  'minionskilled', 
 'neutralMinionsKilledTeamJungle': "neutralminionskilledinteam'sjungle",
 'neutralMinionsKilledEnemyJungle':'neutralminionskilledinenemyjungle',
-     'champion' }
+}
+
+headers_to_add = {'killingSprees': 'killingsprees' ,
+'longestTimeSpentLiving':'longesttimespentliving',
+'damageSelfMitigated':'selfmitigateddamage',
+'visionScore':'visionscore',
+'timeCCingOthers': 'timeccingothers',
+'turretKills':'totalTurretKills',
+'inhibitorKills':'totalInhibitorKills',
+'totalTimeCrowdControlDealt':'totaltimeapplyingcc',
+'firstBloodAssist': 'firstbloodassist',
+'firstTowerKill':'firsttowerkill',
+'firstTowerAssist': 'firsttowerassist',
+'firstInhibitorKill':'firstinhibkill',
+'firstInhibitorAssist':'firstinhibassist',
+'doubleKills': 'doublekills',
+'tripleKills': 'triplekills',
+'quadraKills':'quadrakills',
+'pentaKills':'pentakills'
+}
 
 
 ##########################################
-participantId 1
-win True
-item0 3052
-item1 0
-item2 1055
-item3 3009
-item4 3071
-item5 3812
-item6 3340
-killingSprees 1
-longestTimeSpentLiving 1101
-doubleKills 0
-tripleKills 0
-quadraKills 0
-pentaKills 0
-unrealKills 0
- 14588
- 0
-
- 0
- 7947
-totalUnitsHealed 1
-damageSelfMitigated 10776
- 7149
- 3153
-visionScore 26
-timeCCingOthers 12
- 14880
- 6121
-turretKills 1
-inhibitorKills 1
-totalMinionsKilled 173
-neutralMinionsKilledTeamJungle 7
- 4
-totalTimeCrowdControlDealt 135
-champLevel 14
-3
- 0
- 5
- True
-firstBloodAssist False
-firstTowerKill False
-firstTowerAssist False
-firstInhibitorKill False
-firstInhibitorAssist False
-combatPlayerScore 0
-objectivePlayerScore 0
-totalPlayerScore 0
-totalScoreRank 0
-playerScore0 0
-playerScore1 0
-playerScore2 0
-playerScore3 0
-playerScore4 0
-playerScore5 0
-playerScore6 0
-playerScore7 0
-playerScore8 0
-playerScore9 0
-perk0 8010
-perk0Var1 436
-perk0Var2 0
-perk0Var3 0
-perk1 9111
-perk1Var1 169
-perk1Var2 180
-perk1Var3 0
-perk2 9103
-perk2Var1 0
-perk2Var2 0
-perk2Var3 0
-perk3 8014
-perk3Var1 223
-perk3Var2 0
-perk3Var3 0
-perk4 8139
-perk4Var1 722
-perk4Var2 0
-perk4Var3 0
-perk5 8135
-perk5Var1 944
