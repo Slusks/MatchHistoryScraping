4f339f71270e906dc4085e80904fed00ea1222de
sam <samslusky@gmail.com>
2021-06-26 08:24:56 -0400

    Works again
    There was an issue with the dict keys that wasn't getting shown because I had everything in a try and except block. d'oh

--- MatchHistoryScrapingV2.py
+++ MatchHistoryScrapingV2.py
@@ -16,7 +16,7 @@ import csv
 ###Basically, key = scraped value, value = desired value
     #So I honestly can't remember if this is just filling in holes from Oracle's Elixer tables or if this is supposed to be all information, so it definitely contains some gaps
     #in the information it contains.
-full_headers_dict = {"magicDamageDealt":"totalmagicdamagedealt", "physicalDamageDealt":"totalphysicaldamagedealt", "physicalDamageTaken":"physicaldamagetaken", "kills":"kills","deaths":"deaths","assists":"assists", 'firstBloodKill': 'firstblood', 'totalDamageDealtToChampions': 'totaldamagetochampions', 'physicalDamageDealtToChampions': 'physicaldamagetochampions', 'magicDamageDealtToChampions': 'magicdamagetochampions', 'trueDamageDealtToChampions': 'truedamagetochampions', 'damageDealtToObjectives': 'totaldamagetoobjectives', 'damageDealtToTurrets': 'totaldamagetoturrets', 'totalHeal': 'damagehealed', 'totalDamageTaken': 'damagetaken', 'magicalDamageTaken': 'magicdamagetaken', 'wardsKilled': 'wardsdestroyed', 'sightWardsBoughtInGame': 'stealthwardspurchased', 'visionWardsBoughtInGame': 'controlwardspurchased', 'totalMinionsKilled': 'minionskilled', 'neutralMinionsKilledTeamJungle': "neutralminionskilledinteam'sjungle", 'neutralMinionsKilledEnemyJungle': 'neutralminionskilledinenemyjungle', 'killingSprees': 'killingsprees', 'longestTimeSpentLiving': 'longesttimespentliving', 'damageSelfMitigated': 'selfmitigateddamage', 'visionScore': 'visionscore', 'timeCCingOthers': 'timeccingothers', 'turretKills': 'totalTurretKills', 'inhibitorKills': 'totalInhibitorKills', 'totalTimeCrowdControlDealt': 'totaltimeapplyingcc', 'firstBloodAssist': 'firstbloodassist', 'firstTowerKill': 'firsttowerkill', 'firstTowerAssist': 'firsttowerassist', 'firstInhibitorKill': 'firstinhibkill', 'firstInhibitorAssist': 'firstinhibassist', 'doubleKills': 'doublekills', 'tripleKills': 'triplekills', 'quadraKills': 'quadrakills', 'pentaKills': 'pentakills', 'GoldEarned':'goldearned', 'GoldSpent': 'goldspent', "largestCriticalStrike":"largestcriticalstrike", "largestKillingSpree":"largestkillingspree", "largestMultiKill":"largestmultikill"}
+full_headers_dict = {"magicDamageDealt":"totalmagicdamagedealt", "physicalDamageDealt":"totalphysicaldamagedealt", "physicalDamageTaken":"physicaldamagetaken", "kills":"kills","deaths":"deaths","assists":"assists", 'firstBloodKill': 'firstblood', 'totalDamageDealtToChampions': 'totaldamagetochampions', 'physicalDamageDealtToChampions': 'physicaldamagetochampions', 'magicDamageDealtToChampions': 'magicdamagetochampions', 'trueDamageDealtToChampions': 'truedamagetochampions', 'damageDealtToObjectives': 'totaldamagetoobjectives', 'damageDealtToTurrets': 'totaldamagetoturrets', 'totalHeal': 'damagehealed', 'totalDamageTaken': 'damagetaken', 'magicalDamageTaken': 'magicdamagetaken', 'wardsKilled': 'wardsdestroyed', 'sightWardsBoughtInGame': 'stealthwardspurchased', 'visionWardsBoughtInGame': 'controlwardspurchased', 'totalMinionsKilled': 'minionskilled', 'neutralMinionsKilledTeamJungle': "neutralminionskilledinteam'sjungle", 'neutralMinionsKilledEnemyJungle': 'neutralminionskilledinenemyjungle', 'killingSprees': 'killingsprees', 'longestTimeSpentLiving': 'longesttimespentliving', 'damageSelfMitigated': 'selfmitigateddamage', 'visionScore': 'visionscore', 'timeCCingOthers': 'timeccingothers', 'turretKills': 'totalTurretKills', 'inhibitorKills': 'totalInhibitorKills', 'totalTimeCrowdControlDealt': 'totaltimeapplyingcc', 'firstBloodAssist': 'firstbloodassist', 'firstTowerKill': 'firsttowerkill', 'firstTowerAssist': 'firsttowerassist', 'firstInhibitorKill': 'firstinhibkill', 'firstInhibitorAssist': 'firstinhibassist', 'doubleKills': 'doublekills', 'tripleKills': 'triplekills', 'quadraKills': 'quadrakills', 'pentaKills': 'pentakills', 'goldEarned':'goldearned', 'goldSpent': 'goldspent', "largestCriticalStrike":"largestcriticalstrike", "largestKillingSpree":"largestkillingspree", "largestMultiKill":"largestmultikill"}
 
 #Function that grabs the URL list from a csv
 def get_urllist():
@@ -25,8 +25,8 @@ def get_urllist():
     file = pd.read_csv(url_file, header=0)
     l = list(file.url)
     single = list(set(l))
-    print(len(single))
-    return (single)
+    if len(single) > 5:
+        return (single)
 
 # coding: utf-8
 #!/usr/bin/env python3
@@ -49,9 +49,9 @@ single_url = "https://matchhistory.na.leagueoflegends.com/en/#match-details/ESPO
 
 longer_raw_urllist =['http://matchhistory.na.leagueoflegends.com/en/#match-details/ESPORTSTMNT06/80532?gameHash=ebf3b1a721dc6fde&amp;tab=overview', 'http://matchhistory.na.leagueoflegends.com/en/#match-details/TRLH1/1002210083?gameHash=9ed4c4cb1c26a1ec&amp;tab=overview', 'https://matchhistory.na.leagueoflegends.com/en/#match-details/ESPORTSTMNT02/1384397?gameHash=2c834113f5dc5d1a&amp;tab=overview', 'http://matchhistory.na.leagueoflegends.com/en/#match-details/ESPORTSTMNT06/1180492?gameHash=861f480faed92b0a', 'http://matchhistory.na.leagueoflegends.com/en/#match-details/TREU/1001490197?gameHash=c1d99cbca313dd17&amp;tab=overview', 'http://matchhistory.na.leagueoflegends.com/en/#match-details/ESPORTSTMNT01/1310941?gameHash=d9ada7733493c79c', 'https://matchhistory.na.leagueoflegends.com/en/#match-details/ESPORTSTMNT01/1321280?gameHash=294b476f65218cfe&amp;tab=overview', 'http://matchhistory.na.leagueoflegends.com/en/#match-details/ESPORTSTMNT06/1300358?gameHash=20b575528c3d1ab9', 'https://matchhistory.na.leagueoflegends.com/en/#match-details/TRLH1/1002520246?gameHash=175bf6e0da0ff375', 'http://matchhistory.na.leagueoflegends.com/en/#match-details/ESPORTSTMNT06/170093?gameHash=47e0556f123b2106&amp;tab=overview'] 
 
-table_headers = ["kills","deaths","assists","Largest Killing Spree","Largest Multi Kill","First Blood","Total Damage to Champions","Physical Damage to Champions","Magic Damage to Champions","True Damage to Champions","Total Damage Dealt","Physical Damage Dealt","Magic Damage Dealt","True Damage Dealt","Largest Critical Strike","Total Damage to Objectives","Total Damage to Turrets","Damage Healed","Damage Taken","Physical Damage Taken","Magic Damage Taken","True Damage Taken","Wards Placed","Wards Destroyed","Stealth Wards Purchased","Control Wards Purchased","Gold Earned","Gold Spent","Minions Killed","Neutral Minions Killed","Neutral Minions Killed in Team's Jungle","Neutral Minions Killed in Enemy Jungle","url","champion"]
+#table_headers = ["kills","deaths","assists","Largest Killing Spree","Largest Multi Kill","First Blood","Total Damage to Champions","Physical Damage to Champions","Magic Damage to Champions","True Damage to Champions","Total Damage Dealt","Physical Damage Dealt","Magic Damage Dealt","True Damage Dealt","Largest Critical Strike","Total Damage to Objectives","Total Damage to Turrets","Damage Healed","Damage Taken","Physical Damage Taken","Magic Damage Taken","True Damage Taken","Wards Placed","Wards Destroyed","Stealth Wards Purchased","Control Wards Purchased","Gold Earned","Gold Spent","Minions Killed","Neutral Minions Killed","Neutral Minions Killed in Team's Jungle","Neutral Minions Killed in Enemy Jungle","url","champion"]
 
-scrape2 = r'C:/Users/sam/Desktop/ScrapeTest/scrapeV2.csv'
+
 
 #fixes URL into the form that it needs to be for scraping
 #This will only work for non-LPL matches at this time.
@@ -122,12 +122,11 @@ def get_data(json_content, test):
         outputlist.append(champ_key_dict[str(champion_id)])
         for i in  full_headers_dict: #This is an issue. Didn't actually capture all the possible headers
             if i in headers_list:
-                outputlist.append(json_content["participants"][count]["stats"][i])
+                outputlist.append(json_content["participants"][count]["stats"][i]) #this line doesn't work
         outputlist.append(gameId)
         list_list.append(outputlist)
         count += 1
-    if test == True:
-        pprint.pprint("list_list:", list_list)
+        print('count:', count)
     return (list_list)
 
 #this has to be way worked on with data from LPL decoding
@@ -166,33 +165,35 @@ def combine_csv(match_data, database_file):
 
 
 ###Running Function
-database_file = r'C:/Users/sam/Desktop/ScrapeTest/databaseV2.csv'
-test_database_file = r'C:/Users/sam/Desktop/ScrapeTest/test_databaseV2.csv'
+database_file = r'C:/Users/sam/Desktop/ScrapeTest/databaseV3.csv'
+test_database_file = r'C:/Users/sam/Desktop/ScrapeTest/test_databaseV3.csv'
 temp_file = r'C:/Users/sam/Desktop/ScrapeTest/scrapeV2.csv'
 error_url_file = r'C:/Users/sam/Desktop/ScrapeTest/error_url_list.csv'
-raw_urllist = test_raw_urllist #get_urllist()
+raw_urllist = test_raw_urllist #get_urllist() #
 print("got URLs")
 iteration_count = 0
 start_time = time.time()
 error_url_index =[]
 error_urls =[]
+test = 0
 for url in raw_urllist:
     try:
-        content = get_match_data(url, False)
+        content = get_match_data(url, False, False) #url, test, lpl content
         data_list = get_data(content, True)
+        print('data_list')
         write_to_csv(data_list)
-        combine_csv(temp_file, database_file)
+        combine_csv(temp_file, test_database_file)
         iteration_count = iteration_count + 1
         print("completed:", iteration_count)
         time.sleep(0.5)
     except:
-        print(raw_urllist.index(url))
+        #print('error url:',raw_urllist.index(url))
         error_url_index.append(raw_urllist.index(url))
         error_urls.append(url)
         continue
-#collecting all the urls that failed
-write_to_csv(error_urls, error_url_file )
-#runstats
-print('output_file:', test_database_file)
-print("Run time:", time.time() - start_time )
-print("records:", iteration_count)
+    #collecting all the urls that failed
+    write_to_csv(error_urls, error_url_file )
+    #runstats
+    print('output_file:', test_database_file)
+    print("Run time:", time.time() - start_time )
+    print("records:", iteration_count)
