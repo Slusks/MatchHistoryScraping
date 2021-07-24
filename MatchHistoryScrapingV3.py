3804d73e3817ffcd67dcd23d32189473338e14cb
sam <samslusky@gmail.com>
2021-07-24 18:35:01 -0400

    V3 Working for Test URLs
    Working for pre-2021, non-lpl data.

--- MatchHistoryScrapingV3.py
+++ MatchHistoryScrapingV3.py
@@ -71,7 +71,7 @@ for i in champ_name:
 #pprint.pprint(champ_dd.json())
 
 ##PULLING DATA###
-#get request that needs to be looped
+####### Pre 2021, Non-LPL data ######
 def get_match_data(url, test):
     good_url = url_for_request_scraping(url)
     json_file = requests.get(good_url, cookies=user_cookie)
@@ -81,11 +81,10 @@ def get_match_data(url, test):
         pprint.pprint(json_content)
     return(json_content)
 
-def format_data(input_match_data, output_pandas_df, test):
+def build_dataframe(input_match_data, test):
     json_content = input_match_data
     count = 0
     while count < 10:
-        player = json_content["participantIdentities"][count]["player"]["summonerName"]
         champion_id = json_content["participants"][count]["championId"]
         champion_name = champ_key_dict[str(champion_id)]
         gameId = json_content["gameId"]
@@ -93,20 +92,93 @@ def format_data(input_match_data, output_pandas_df, test):
         stats.update({"championId": champion_name , "gameId": gameId})
         if count == 0:
             df = pd.DataFrame([stats])
+            count = count + 1
         else:
             new_df = pd.DataFrame([stats])
-            df = df.append(new_df)
-        count = count + 1
-  
-        return(df)
-      
+            count = count + 1
+            df = df.append(new_df) 
+    if test == True:
+        pprint.pprint(df)
+    return(df)
 
-test_database_file = r'C:/Users/sam/Desktop/ScrapeTest/test_databaseV3.csv'
+def prune_dataframe(input_raw_dataframe, test):
+    bad_df = input_raw_dataframe
+    all_columns = bad_df.columns.values.tolist()
+    bad_keys = []
+    for i in all_columns:
+        if i not in scrape_headers:
+            bad_keys.append(i)
+    good_df = bad_df.drop(bad_keys, axis=1)
+    if test == True:
+        pprint.pprint(good_df)    
+    return (good_df)
+#######################################
 
-for url in raw_urllist:
-    try:
+### Writing Data 
+#write lists to temp CSV
+def write_to_csv(data_list, output_file):
+    dataframe = pd.DataFrame(data_list)
+    dataframe.to_csv(output_file, index=False, header=True) # print to our csv
+
+#add content of temp csv file to database csv.
+def combine_csv(match_data, database_file, iteration_count, test):
+    print('combining csv')
+    print(iteration_count)
+    if iteration_count == 0:
+        with open(database_file, 'w+') as database_file:
+            match_data_container = pd.read_csv(match_data, header=0, delimiter=',', encoding="utf-8-sig")
+            match_data_container.to_csv(database_file, mode="a", index=False)
+    else:
+        match_data_container = pd.read_csv(match_data, header=0, skiprows=[0], delimiter=',',encoding="utf-8-sig")
+        match_data_container.to_csv(database_file, mode="a", index=False)
+
+
+on_Laptop = False
+
+# if on_Laptop:
+#     database_file = r'C:/Users/samsl/Desktop/ScrapeTest/databaseV3.csv'
+#     test_database_file = r'C:/Users/samsl/Desktop/ScrapeTest/test_databaseV3.csv'
+#     temp_file = r'C:/Users/samsl/Desktop/ScrapeTest/scrapeV2.csv'
+
+#     database_file_lpl = r'C:/Users/samsl/Desktop/ScrapeTest/databaseV3_lpl.csv'
+#     test_database_file_lpl = r'C:/Users/samsl/Desktop/ScrapeTest/test_databaseV3_lpl.csv'
+#     temp_file_lpl = r'C:/Users/samsl/Desktop/ScrapeTest/scrapeV2_lpl.csv'
+
+#     master_file = r'C:/Users/samsl/Desktop/ScrapeTest/master_combined_database.csv'
+
+#     error_url_file = r'C:/Users/samsl/Desktop/ScrapeTest/error_url_list.csv'    
+# elif not on_Laptop:
+#     database_file = r'C:/Users/sam/Desktop/ScrapeTest/databaseV3.csv'
+#     test_database_file = r'C:/Users/sam/Desktop/ScrapeTest/test_databaseV3.csv'
+#     temp_file = r'C:/Users/sam/Desktop/ScrapeTest/scrapeV2.csv'
+
+#     database_file_lpl = r'C:/Users/sam/Desktop/ScrapeTest/databaseV3_lpl.csv'
+#     test_database_file_lpl = r'C:/Users/samsl/Desktop/ScrapeTest/test_databaseV3_lpl.csv'
+#     temp_file_lpl = r'C:/Users/sam/Desktop/ScrapeTest/scrapeV2_lpl.csv'
+
+#     master_file = r'C:/Users/sam/Desktop/ScrapeTest/master_combined_database.csv'
+
+#     error_url_file = r'C:/Users/sam/Desktop/ScrapeTest/error_url_list.csv'
+
+
+#### RUNNING FILES ##########
+urllist = raw_urllist #setting this as a variable so i can switch between this and other urllists      
+test_match_file = r'C:/Users/sam/Desktop/ScrapeTest/test_match_file_V3.csv'
+test_database_file = r'C:/Users/sam/Desktop/ScrapeTest/test_database_V3.csv'
+iteration_count = 0
+start_time = time.time()
+for url in urllist:
+    try: #this currently works for Pre-2020, non-lpl urls and match data
         full_match_data = get_match_data(url, False)
-        x = format_data(full_match_data, test_database_file, True)
+        long_match_dataframe = build_dataframe(full_match_data, True)
+        short_match_dataframe = prune_dataframe(long_match_dataframe, False)
+        short_match_dataframe.rename(columns = full_headers_dict)
+        write_to_csv(short_match_dataframe, test_match_file)
+        combine_csv(test_match_file, test_database_file, iteration_count, False)
+        iteration_count = iteration_count + 1
     except Exception as e:
         print(e) 
-print(x)
+    
+ 
+print("Run time:", time.time() - start_time )
+print("records:", iteration_count)
